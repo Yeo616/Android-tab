472e2ec936bd3c5a5739d49d8709bc4a64334811
Yeo <guswls9281@gmail.com>
2022-07-27 17:57:11 +0900

    0727

---
+++ app/src/main/java/com/yeo/tab/MainActivity.java
@@ -0,0 +1,82 @@
+package com.yeo.tab;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView navigationView;
+
+    Fragment firstFragment;
+    Fragment secondFragment;
+    Fragment thirdFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 액션바 숨기는 코드
+//        getSupportActionBar().hide();
+        getSupportActionBar().setTitle("홈");
+        navigationView = findViewById(R.id.bottomNavigationView);
+
+        firstFragment = new FirstFragment();
+        secondFragment = new SecondFragment();
+        thirdFragment = new ThirdFragment();
+
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int itemId = item.getItemId();
+
+                Fragment fragment = null;
+
+                if(itemId == R.id.firstFragment){
+                    fragment = firstFragment;
+                    getSupportActionBar().setTitle("홈");
+                    getSupportActionBar().show();
+                } else if(itemId == R.id.secondFragment){
+                    fragment = secondFragment;
+
+                    getSupportActionBar().setTitle("친구");
+                    getSupportActionBar().show();
+
+                } else if(itemId == R.id.thirdFragment){
+                    fragment = thirdFragment;
+
+                    getSupportActionBar().hide();
+
+                }
+                return loadFragment(fragment);
+            }
+        });
+
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if(fragment != null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+}
+
+
+
+
+
